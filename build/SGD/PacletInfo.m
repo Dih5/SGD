7805d948e7d5d700d7bcbf619d4ebc4ffca64993
Guillermo Hernández <Dih5@users.noreply.github.com>
2015-06-08 12:23:39 +0200

    First commit

---
+++ build/SGD/PacletInfo.m
@@ -0,0 +1,11 @@
+(* Paclet Info File *)
+
+(* created 2015.06.01*)
+
+Paclet[
+  Name -> "SGD",
+  Version -> "0.0.1",
+  MathematicaVersion -> "6+",
+  Extensions -> {
+    {"Documentation", Language -> "English"}
+}]
